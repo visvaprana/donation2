b0d4a00cefbb41fb6016686349e3123d4913bb54
visvaprana <visvaprananimaidas.jps@gmail.com>
2026-08-19 20:09:07 +0600

    Add MetaDataResponse and related DTO classes

--- app/src/main/java/com/matchlessgiftikd/donation/data/remote/MetaDataResponse.kt
+++ app/src/main/java/com/matchlessgiftikd/donation/data/remote/MetaDataResponse.kt
@@ -1,1 +1,28 @@
+package com.matchlessgiftikd.donation.data.remote
 
+import com.google.gson.annotations.SerializedName
+
+data class MetaDataResponse(
+    @SerializedName("success")
+    val success: Boolean,
+    @SerializedName("districts")
+    val districts: List<DistrictDto>,
+    @SerializedName("thanas")
+    val thanas: List<ThanaDto>? = emptyList()
+)
+
+data class DistrictDto(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
+)
+
+data class ThanaDto(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("district_id")
+    val districtId: Int,
+    @SerializedName("name")
+    val name: String
+)
