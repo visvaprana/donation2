8146b8ec7bb30f109a7f6ab0bccde4ebd55a53e7
visvaprana <visvaprananimaidas.jps@gmail.com>
2026-08-19 18:53:13 +0600

    Add PromisedDonationEntity to AppDatabase

--- app/src/main/java/com/matchlessgiftikd/donation/data/local/AppDatabase.kt
+++ app/src/main/java/com/matchlessgiftikd/donation/data/local/AppDatabase.kt
@@ -5,10 +5,20 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [DonationEntity::class, DistrictEntity::class, ThanaEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        DonationEntity::class, 
+        DistrictEntity::class, 
+        ThanaEntity::class,
+        PromisedDonationEntity::class
+    ], 
+    version = 1, 
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun donationDao(): DonationDao
     abstract fun metaDataDao(): MetaDataDao
+    abstract fun promisedDonationDao(): PromisedDonationDao
 
     companion object {
         @Volatile
