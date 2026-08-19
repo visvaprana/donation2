0d35746604495e53466dfa39a1a01b16302347b5
visvaprana <visvaprananimaidas.jps@gmail.com>
2026-08-19 18:42:18 +0600

    Add Material Components and AndroidX Core dependencies
    Added Material Components and AndroidX Core dependencies to fix build errors.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,12 @@ android {
 }
 
 dependencies {
+    // Material Components & AndroidX Core (Fixes build error)
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+
     // Room Database
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
