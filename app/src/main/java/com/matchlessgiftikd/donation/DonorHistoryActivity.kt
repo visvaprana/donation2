d948e287158ac7bf3018c432b5fc2ef91f8ebb3b
visvaprana <visvaprananimaidas.jps@gmail.com>
2026-08-20 23:14:50 +0600

    Create DonorHistoryActivity for donor history screen

---
+++ app/src/main/java/com/matchlessgiftikd/donation/DonorHistoryActivity.kt
@@ -0,0 +1,13 @@
+package com.matchlessgiftikd.donation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DonorHistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_donor_history)
+    }
+}
