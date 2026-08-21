205985b1576e70cd98331ec6ac0e85fd394ffa33
visvaprana <visvaprananimaidas.jps@gmail.com>
2026-08-21 11:09:06 +0600

    Remove button listeners from DashboardActivity
    Removed button click listeners for various donation activities.

--- app/src/main/java/com/matchlessgiftikd/donation/DashboardActivity.kt
+++ app/src/main/java/com/matchlessgiftikd/donation/DashboardActivity.kt
@@ -1,8 +1,6 @@
 package com.matchlessgiftikd.donation
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class DashboardActivity : AppCompatActivity() {
@@ -11,35 +9,5 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_dashboard)
-
-        findViewById<Button>(R.id.btnDonation).setOnClickListener {
-            startActivity(
-                Intent(this, DonationActivity::class.java)
-            )
-        }
-
-        findViewById<Button>(R.id.btnPromisedDonation).setOnClickListener {
-            startActivity(
-                Intent(this, PromisedDonationActivity::class.java)
-            )
-        }
-
-        findViewById<Button>(R.id.btnDonations).setOnClickListener {
-            startActivity(
-                Intent(this, DonationListActivity::class.java)
-            )
-        }
-
-        findViewById<Button>(R.id.btnPromisedDonations).setOnClickListener {
-            startActivity(
-                Intent(this, PromisedDonationListActivity::class.java)
-            )
-        }
-
-        findViewById<Button>(R.id.btnDonorHistory).setOnClickListener {
-            startActivity(
-                Intent(this, DonorHistoryActivity::class.java)
-            )
-        }
     }
 }
